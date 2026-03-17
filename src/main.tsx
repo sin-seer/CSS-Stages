8b8d1c4cdb2b2eafa4b0990701d338116f64f291
sin.seer <127638127+sin-seer@users.noreply.github.com>
2026-03-16 18:07:24 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StageBase from './stage'
+import Stage from './Stage'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -10,6 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   //     commented-out code, but because of the nature
   //     of stages it's disabled by default here.
   // <React.StrictMode>React.StrictMode>
-    <StageBase />
+    <Stage />
   //</React.StrictMode>,
 )
