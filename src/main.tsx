6259f0c21693e38613db6571639c19cf0d0a000f
sin.seer <127638127+sin-seer@users.noreply.github.com>
2026-03-16 18:05:02 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import stage from './stage'
+import Stagebase from './stage'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
